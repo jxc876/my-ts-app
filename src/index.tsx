9971d245dc7ea2721a1931f1f9146bb846593cc0
Michael Ramirez <Michael.Ramirez@target.com>
2023-11-14 10:05:21 -0600

    Move App into components folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 // Routing
@@ -16,8 +14,10 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import './index.css';
 
 // Components
+import App from './components/App';
 import { Component3 } from './components/Component3';
 import HelloWorld from './components/HelloWorld';
 
